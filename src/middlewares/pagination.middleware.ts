68a4de222eab309aaf956ccd25c55c035efd7f29
Dung2710 <dungpdph50412@gmail.com>
2026-03-08 08:57:39 +0700

    cập nhật utils nền tảng

--- src/middlewares/pagination.middleware.ts
+++ src/middlewares/pagination.middleware.ts
@@ -1,8 +1,9 @@
-import type { NextFunction, Request, Response } from 'express';
 
 import { env } from '@config/env';
 import { parsePagination } from '@utils/pagination';
+import type { NextFunction, Request, Response } from 'express';
 
+// worklog: 2026-03-04 12:10:42 | vanduc | cleanup | parsePaginationMiddleware
 export const parsePaginationMiddleware = (req: Request, res: Response, next: NextFunction) => {
   res.locals.pagination = parsePagination(req.query, {
     page: env.PAGINATION_DEFAULT_PAGE,
