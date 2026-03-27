da4e31128b3070bfb0c4b428a48ec30b2216b269
vanduc04 <buiduc1709@gmail.com>
2026-03-27 13:23:12 +0700

    api chatbot

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import authRouter from '@routes/auth.route';
 import healthRouter from '@routes/health.route';
 import { Router } from 'express';
 import categoryRouter from './category.route';
+import chatbotRouter from './chatbot.route';
+import pushNotificationRouter from './push-notification.route';
 
 const apiRouter = Router();
 
@@ -9,4 +11,6 @@ apiRouter.use('/health', healthRouter);
 apiRouter.use('/auth', authRouter);
 
 apiRouter.use('/categories', categoryRouter);
+apiRouter.use('/chatbot', chatbotRouter);
+apiRouter.use('/notifications', pushNotificationRouter);
 export default apiRouter;
