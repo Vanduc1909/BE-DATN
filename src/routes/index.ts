78a65fb506157f88a9304932a07b7e9e18a165d3
vanduc04 <buiduc1709@gmail.com>
2026-03-30 13:09:17 +0700

    Kích hoạt route chat hỗ trợ khách hàng

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,17 @@
 import authRouter from '@routes/auth.route';
 import healthRouter from '@routes/health.route';
 import { Router } from 'express';
-import categoryRouter from './category.route';
-import chatbotRouter from './chatbot.route';
-import pushNotificationRouter from './push-notification.route';
-
+import categoryRouter from '@routes/category.route';
+import chatbotRouter from '@routes/chatbot.route';
+import pushNotificationRouter from '@routes/pushNotification.route';
+import chatRouter from '@routes/chat.route';
 const apiRouter = Router();
 
 apiRouter.use('/health', healthRouter);
 apiRouter.use('/auth', authRouter);
 
 apiRouter.use('/categories', categoryRouter);
+apiRouter.use('/chat', chatRouter);
 apiRouter.use('/chatbot', chatbotRouter);
 apiRouter.use('/notifications', pushNotificationRouter);
 export default apiRouter;
